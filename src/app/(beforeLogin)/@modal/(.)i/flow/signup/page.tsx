cf4ba292f07bc9de6e8cbed92673552b40b9fa13
ParkKiHun <miy1205@naver.com>
2024-05-26 20:46:36 +0900

    Initial Commit

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupModal from "@/app/(beforeLogin)/_component/SingupModal";
+
+export default function Home() {
+  return (
+    <>
+      난 가로채기
+      <SignupModal />
+    </>
+  );
+}
---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Main from "../_component/Main";
+
+export default function Login() {
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return <Main />;
+}
+
+/* Redirect 방법 2
+  1. ServerComponent : 
+    import { redirect } from "next/navigation";
+    redirect("/i/flow/login");
+
+  2. ClientComponent
+    import { useRouter } from "next/navigation";
+    const router = useRouter();
+    router.replace("/i/flow/login");
+    return null;
+*/
+
+// router.push
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+// 뒤로가기 : localhost:3000/login
+
+// router.replace
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+// 뒤로가기 : localhost:3000
